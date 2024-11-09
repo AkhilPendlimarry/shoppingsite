95d01dfe2152ca4ff0b0677ea1836ebf2644e873
AkhilPendlimarry <127538647+AkhilPendlimarry@users.noreply.github.com>
2024-11-09 16:47:54 +0530

    Add files via upload

---
+++ src/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+  <>
+    <div className="heading">
+      <h1>Shopping Site</h1>
+      <a href="/AllProducts">Products</a>
+      <a href="/cart">Cart</a>
+    </div>
+
+  </>
+ )
+}
+
